cf20e061fb8efcabe7dba8d44720c3e35b699c1c
kokainaW <williamkhoko@yahoo.com>
2023-08-29 22:28:35 +0100

    104-binary_tree_rotate_right.c

---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_right - For right rotation on a tree
+ * @tree: A pointer to the tree
+ * Return: A pointer
+ */
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+	binary_tree_t *chl, *parent = tree;
+
+	if (!tree)
+		return (NULL);
+
+	chl = parent->left;
+	if (!chl)
+		return (tree);
+
+	if (chl->right)
+		chl->right->parent = parent;
+
+	parent->left = chl->right;
+	chl->right = parent;
+	chl->parent = parent->parent;
+	parent->parent = chl;
+
+	if (chl->parent && chl->parent->left == parent)
+		chl->parent->left = chl;
+	else if (chl->parent)
+		chl->parent->right = chl;
+
+	return (chl);
+}
