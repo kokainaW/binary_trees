d2d03529c48199bcb1bcddf827a06d28faedd6dd
kokainaW <williamkhoko@yahoo.com>
2023-08-29 22:00:25 +0100

    100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -4,7 +4,7 @@
  * binary_trees_ancestor - It finds the lowest node
  * @first: A pointer to the  1st node 
  * @second: A pointer to the 2nd node
- * Return: A pointer to the lowest node or NULL
+ * Return: NULL
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 					const binary_tree_t *second)
