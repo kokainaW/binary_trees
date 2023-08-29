84eacf4d4f252bd9c20314a9d414694c67f940d2
kokainaW <williamkhoko@yahoo.com>
2023-08-29 21:57:23 +0100

    100-binary_trees_ancestor.c

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - It finds the lowest node
+ * @first: A pointer to the  1st node 
+ * @second: A pointer to the 2nd node
+ *
+ * Return: A pointer to the lowest node or NULL
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+					const binary_tree_t *second)
+{
+	const binary_tree_t *f_anc, *s_anc;
+
+	for (f_anc = first; f_anc; f_anc = f_anc->parent)
+		for (s_anc = second; s_anc; s_anc = s_anc->parent)
+			if (f_anc == s_anc)
+				return ((binary_tree_t *)f_anc);
+	return (NULL);
+}
